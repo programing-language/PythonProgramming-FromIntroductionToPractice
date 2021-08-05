f995037d4db944d3b21aca904bd32d89ae6839f4
cjx <1067446576@qq.com>
2021-08-05 14:56:26 +0800

    str

---
+++ partI.basic.knowledge/chapterII.variables_and_simple_data_types/sectionIV.number/str.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: str.py
+@time: 2021/8/5 下午2:55
+@desc:
+'''
+age = 23
+message = "Happy " + str(age) + "rd Birthday!"
+
+print(message)
