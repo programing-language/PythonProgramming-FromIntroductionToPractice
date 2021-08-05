34c723399912bde9d328e09345b196a9e99b13ef
cjx <1067446576@qq.com>
2021-08-05 14:38:47 +0800

    string apostrophe

---
+++ partI.basic.knowledge/chapterII.variables_and_simple_data_types/apostrophe.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: apostrophe.py
+@time: 2021/8/5 下午2:34
+@desc:
+'''
+message = "One of Python's strengths is its diverse community."
+print(message)
