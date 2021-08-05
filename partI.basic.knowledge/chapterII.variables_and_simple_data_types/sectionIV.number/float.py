5ce8dd771f1d00b2d71eb62ed077675c84451a7b
cjx <1067446576@qq.com>
2021-08-05 14:54:05 +0800

    float

---
+++ partI.basic.knowledge/chapterII.variables_and_simple_data_types/sectionIV.number/float.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: float.py
+@time: 2021/8/5 下午2:51
+@desc:
+'''
+print(0.1 + 0.1)
+print(0.2 + 0.2)
+print(2 * 0.1)
+print(2 * 0.2)
+
+print(0.2 + 0.1)
+print(3 * 0.1)
