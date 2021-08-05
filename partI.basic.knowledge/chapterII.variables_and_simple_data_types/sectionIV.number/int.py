82d4b99a19d3830cab3e368fb4a9ebd79aea5b94
cjx <1067446576@qq.com>
2021-08-05 14:51:06 +0800

    int

---
+++ partI.basic.knowledge/chapterII.variables_and_simple_data_types/sectionIV.number/int.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: int.py
+@time: 2021/8/5 下午2:49
+@desc:
+'''
+print(2 + 3)
+print(2 - 3)
+print(2 * 3)
+print(2 / 3)
+
+print(3 ** 2)
+print(3 ** 3)
+print(10 ** 6)
+
+print(2 + 3 * 4)
+print((2 + 3) * 4)
