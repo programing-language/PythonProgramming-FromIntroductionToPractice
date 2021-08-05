cb7ddfda516f185bbf1be20447ba4116ce3a5e5d
cjx <1067446576@qq.com>
2021-08-05 11:03:38 +0800

    variables

---
+++ partI.basic.knowledge/chapterII.variables_and_simple_data_types/variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: hello_world.py
+@time: 2021/8/5 上午10:53
+@desc:
+'''
+message = "Hello Python world!"
+print(message)
+
+message = "Hello Python Crash Course world!"
+print(message)
