2c2232939124e73b30096dc3a3c19f437cc5190f
cjx <1067446576@qq.com>
2021-08-05 16:03:40 +0800

    list

---
+++ partI.basic.knowledge/chapterIII.list_introduction/what_is_the_list/bicycles.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: bicycles.py
+@time: 2021/8/5 下午3:41
+@desc:
+'''
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+print(bicycles)
+print(bicycles[0])
+print(bicycles[0].title())
+
+# 倒数索引
+print(bicycles[-1])
+print(bicycles[-3])
