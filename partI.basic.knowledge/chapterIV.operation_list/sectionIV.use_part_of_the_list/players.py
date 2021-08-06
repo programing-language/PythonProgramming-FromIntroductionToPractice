4ce01b94f1ac5c28ee44cbbfcc360b630e04b47d
cjx <1067446576@qq.com>
2021-08-06 16:10:04 +0800

    列表切片

--- partI.basic.knowledge/chapterIV.operation_list/sectionIV.use_part_of_the_list/players.py
+++ partI.basic.knowledge/chapterIV.operation_list/sectionIV.use_part_of_the_list/players.py
@@ -6,3 +6,16 @@
 @time: 2021/8/6 下午4:01
 @desc:
 '''
+# 切片: [n:m] -> [n, m)
+players = ['charles', 'martina', 'michael', 'florence', 'eli']
+print(players[0:3])
+
+# 不指定第一个索引，则自动从列表开头开始
+print(players[:3])
+
+# 不指定第二个索引，则自动截止到列表结尾
+print(players[3:])
+
+# 遍历切片: 输出名单上的最后三名队员
+for player in players[-3:]:
+    print(player)
