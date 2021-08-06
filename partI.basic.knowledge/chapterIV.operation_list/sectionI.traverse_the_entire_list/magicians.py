9379794a1bd4edf69e28cc861a1e27666e410977
cjx <1067446576@qq.com>
2021-08-06 15:19:17 +0800

    遍历列表

---
+++ partI.basic.knowledge/chapterIV.operation_list/sectionI.traverse_the_entire_list/magicians.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: chenjinxin
+@file: magicians.py
+@time: 2021/8/6 下午2:56
+@desc:
+'''
+magicians = ["alice", 'david', 'carolina']
+for magician in magicians:
+    print(magician)
