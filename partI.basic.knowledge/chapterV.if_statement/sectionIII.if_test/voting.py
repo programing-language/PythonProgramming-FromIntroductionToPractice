82554d762f544f21f961fd9e52c522ff430c9e3b
cjx <1067446576@qq.com>
2021-08-26 16:44:03 +0800

    if

---
+++ partI.basic.knowledge/chapterV.if_statement/sectionIII.if_test/voting.py
@@ -0,0 +1,54 @@
+print("------------ if -------------")
+age = 19
+if age >= 18:
+    print("You are old enough to vote!")
+
+print("------------ if else -------------")
+age = 17
+if age >= 18:
+    print("You are old enough to vote!")
+    print("Have you registered to vote yet?")
+else:
+    print("Sorry, you are too young to vote.")
+    print("Please register to vote as soon as you turn 18!")
+
+print("------------ if elif else -------------")
+age = 12
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+else:
+    price = 10
+print("Your admission cost is $" + str(price) + ".")
+
+print("------------ if elif... else -------------")
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+else:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
+
+print("------------ 省略else代码块 -------------")
+# Python并不要求if-elif结构后面必须有else代码块。在有些情况下，else代码块很有用；而在其他一些情况下，使用一条elif语句来处理特定的情形更清晰：
+age = 12
+
+if age < 4:
+    price = 0
+elif age < 18:
+    price = 5
+elif age < 65:
+    price = 10
+elif age >= 65:
+    price = 5
+
+print("Your admission cost is $" + str(price) + ".")
+# else是一条包罗万象的语句，只要不满足任何if或elif中的条件测试，其中的代码就会执行，这可能会引入无效甚至恶意的数据。
+# 如果知道最终要测试的条件，应考虑使用一个elif代码块来代替else代码块。这样，你就可以肯定，仅当满足相应的条件时，你的代码才会执行。
