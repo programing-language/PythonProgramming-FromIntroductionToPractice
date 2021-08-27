33a98128f88f23a56dc89c064eb07720ca7af37c
cjx <1067446576@qq.com>
2021-08-27 13:27:11 +0800

    位置实参

---
+++ partI.basic.knowledge/chapterVIII.function/sectionII.pass_arguments/pets.py
@@ -0,0 +1,10 @@
+# 位置实参
+def describe_pet(animal_type, pet_name):
+    """显示宠物的信息"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet('hamster', 'harry')
+# 调用函数多次
+describe_pet('dog', 'willie')
