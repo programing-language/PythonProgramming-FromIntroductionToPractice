e8a10289b3cc619dc317f0cbd6b552d466617e8d
cjx <1067446576@qq.com>
2021-08-27 10:37:47 +0800

    使用标志设置 while 退出

---
+++ partI.basic.knowledge/chapterVII.user_input_and_while_loop/sectionII.while_loop/parrot2.py
@@ -0,0 +1,12 @@
+# 使用标志设置 while 退出
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program. "
+
+active = True
+while active:
+    message = input(prompt)
+
+    if message == 'quit':
+        active = False
+    else:
+        print(message)
