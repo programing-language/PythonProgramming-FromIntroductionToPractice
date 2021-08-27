fae51d14811c9f21331a5c01e32c66827c719992
cjx <1067446576@qq.com>
2021-08-27 13:29:37 +0800

    关键字实参

---
+++ partI.basic.knowledge/chapterVIII.function/sectionII.pass_arguments/pets2.py
@@ -0,0 +1,8 @@
+# 关键字实参
+def describe_pet(animal_type, pet_name):
+    """显示宠物的信息"""
+    print("\nI have a " + animal_type + ".")
+    print("My " + animal_type + "'s name is " + pet_name.title() + ".")
+
+
+describe_pet(animal_type='hamster', pet_name='harry')
