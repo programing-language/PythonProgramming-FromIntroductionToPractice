9005cf0692fb9ce0557df4cccf52f9ca25ebb9a3
cjx <1067446576@qq.com>
2021-08-27 15:06:05 +0800

    使用任意数量的关键字实参

---
+++ partI.basic.knowledge/chapterVIII.function/sectionV.pass_any_number_of_arguments/user_profile.py
@@ -0,0 +1,18 @@
+# 使用任意数量的关键字实参
+# 形参**user_info中的两个星号让Python创建一个名为user_info的空字典，并将收到的所有名称—值对都封装到这个字典中。
+
+
+def build_profile(first, last, **user_info):
+    """创建一个字典，其中包含我们知道的有关用户的一切"""
+    profile = {}
+    profile['first_name'] = first
+    profile['last_name'] = last
+    for key, value in user_info.items():
+        profile[key] = value
+    return profile
+
+
+user_profile = build_profile('albert', 'einstein',
+                             location='princeton',
+                             field='physics')
+print(user_profile)
