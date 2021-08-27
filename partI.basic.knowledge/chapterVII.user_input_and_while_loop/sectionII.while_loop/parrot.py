17d481fa6271149fc3745cdc1d4b870a641d8b10
cjx <1067446576@qq.com>
2021-08-27 10:35:13 +0800

    设置 while 退出

---
+++ partI.basic.knowledge/chapterVII.user_input_and_while_loop/sectionII.while_loop/parrot.py
@@ -0,0 +1,8 @@
+# 设置 while 退出
+prompt = "\nTell me something, and I will repeat it back to you:"
+prompt += "\nEnter 'quit' to end the program: "
+message = ""
+while message != 'quit':
+    message = input(prompt)
+    if message != 'quit':
+        print(message)
